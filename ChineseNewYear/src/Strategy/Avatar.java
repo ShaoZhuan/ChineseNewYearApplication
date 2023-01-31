db56a82cf7e46b8255951f301f0885b71fd9954c
Hng Shao Zhuan <hng.567123@gmail.com>
2023-02-01 02:43:54 +0800

    change strategy

--- ChineseNewYear/src/Strategy/Avatar.java
+++ ChineseNewYear/src/Strategy/Avatar.java
@@ -1,5 +1,26 @@
 package Strategy;
 
-public interface Avatar {           
-            public abstract void performMove(boolean standing);                   
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public abstract class Avatar {
+    
+    MoveBehaviour moveBehavior; 
+    ImageView iv; 
+    Image img;        
+    
+    public void toggle(boolean standing){
+        if(standing){
+            this.moveBehavior = new NoWalk();
+        }else{
+            this.moveBehavior = new CanWalk();
+        }        
+        setImg();
+    }
+    
+    public abstract void setImg();
+        
+    public void performMove(){
+        this.moveBehavior.move(iv, img);
+    }
 }
--- ChineseNewYear/src/Strategy/Reindeer.java
+++ ChineseNewYear/src/Strategy/Reindeer.java
@@ -1,28 +1,22 @@
 package Strategy;
 
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import res.Index.*;
 
-public class Reindeer implements Avatar {
+public class Reindeer extends Avatar {
     
-    private final ImageView iv;
-    private final Image stand = PRESET.PRESET1.getImage(5);
-    private final Image walk = PRESET.PRESET1.getImage(6);
-    MoveBehaviour walking, standing;
     public Reindeer(ImageView iv) {
         this.iv = iv;
-        walking = new CanWalk();
-        standing = new NoWalk();
     }
-
+    
+    
     @Override
-    public void performMove(boolean isStanding) {
-        if(isStanding){
-            walking.move(iv, walk);
+    public void setImg(){
+        if(moveBehavior instanceof NoWalk){
+            this.img = PRESET.PRESET1.getImage(5);
         }
         else{
-            standing.move(iv, stand);
+            this.img = PRESET.PRESET1.getImage(6);            
         }
     }
 
